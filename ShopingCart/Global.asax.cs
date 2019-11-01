bd26f5a73cc2e8c914373aa1fcbdfcfe4f4d868c
nmnhat <nmnhat@cmc.com.vn>
2019-11-01 17:32:07 +0700

    update code last

--- ShopingCart/Global.asax.cs
+++ ShopingCart/Global.asax.cs
@@ -18,24 +18,24 @@ namespace ShopingCart
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-		//protected void Application_Error(object sender, EventArgs e)
-		//{
-		//	var errors = Server.GetLastError();
+		protected void Application_Error(object sender, EventArgs e)
+		{
+			var errors = Server.GetLastError();
 
-		//	var error = Server.GetLastError() as HttpException;
-		//	if (errors != null || error != null && !string.IsNullOrWhiteSpace(error.Message))
-		//	{
-		//		if (error != null && error.GetHttpCode() == 404)
-		//		{
-		//			Server.ClearError();
-		//			Context.Response.Redirect("/Home/Error404");
-		//		}
-		//		else
-		//		{
-		//			Context.ClearError();
-		//			Context.Response.Redirect("/Home/Error500");
-		//		}
-		//	}
-		//}
+			var error = Server.GetLastError() as HttpException;
+			if (errors != null || error != null && !string.IsNullOrWhiteSpace(error.Message))
+			{
+				if (error != null && error.GetHttpCode() == 404)
+				{
+					Server.ClearError();
+					Context.Response.Redirect("/Home/Error404");
+				}
+				else
+				{
+					Context.ClearError();
+					Context.Response.Redirect("/Home/Error500");
+				}
+			}
+		}
 	}
 }
